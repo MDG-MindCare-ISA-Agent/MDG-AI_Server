0bfe53d9c6bd68b5d6dd9266aa77894dcf6191a1
taejun0 <ohtaetaejun@gmail.com>
2025-08-07 01:44:48 +0900

    [Fix] 배포 설정

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def run_api():
         return jsonify({"ok": False, "error": str(e)}), 400
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
